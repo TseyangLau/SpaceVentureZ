098fb45f6e45768a17d7e98421435ebed349f878
isabel2296 <70250018+isabel2296@users.noreply.github.com>
2020-09-14 10:54:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,69 @@
-import sys
+# import sys
 import pygame
+from pygame.locals import *
 from shipbase import Ship
+from display import Display
 
-class SpaceVentureZ:
+class SpaceVentureZ(Display):
     def __init__(self):
-        pygame.init()  # initiate pygame
-        # window size
-        background_color = (0, 0, 0)  # background change to black
-        (width, height) = (600, 600)
-        self.window = pygame.display.set_mode((width, height))
-        self.window.fill(background_color)
-        pygame.display.set_caption('SpaceVentureZ')
+
+        Display.__init__(self)  # keeps the inheritance of Display
         # set window icon
         # icon = pygame.image.load('...')
         # pygame.display.set_icon(icon)
-    def menu(self):
-        pass #tbd will work on this
+        # game variables
+        self.playerLifeCount = 100
+
     def run_game(self):
-        #testing only
-        player_ship = Ship(100, 400, 400, 50)
-        player_ship.draw(self.window)
-        #end of testing
-        running = True
-        while running:
+        start = True
+        while start:
+            self.display.fill((0, 0, 0))
+            self.add_text('Game Page', self.font, (255, 0, 0), self.display, 100, 0)
+
+            # mouse coordinates
+            x, y = pygame.mouse.get_pos()
+
+            # Buttons
+            menu = pygame.Rect(0, 0, 80, 40)
+
+            # if buttons are clicked actions
+            if menu.collidepoint(x, y):
+                if self.user_click:
+                    self.user_click = False
+                    self.menu()
+            # Draw Button
+            pygame.draw.rect(self.display, (192, 192, 192), menu)
+            self.add_text('Menu', self.font, (255, 255, 255), self.display, 0, 5)
+
+            '''RUN GAME HERE'''
+
+            player_ship = Ship(100, 400, 400, 50)
+            player_ship.draw(svz.display)
+
+            # *************
+
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+                if event.type == QUIT:
+                    pygame.quit()
+                    start = False
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        start = False
+                if event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.user_click = True
             pygame.display.update()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # create game instance and run it
     svz = SpaceVentureZ()
-    svz.run_game()
+    svz.menu()
+
+''' 
+run_game() in SpaceVentureZ overwrites the run_game in Display()
+Note: place all game instances and functions for the game in run_game()
+there is an example already placed
+
+self.display is the window (changed the name from self.window) 
+'''
