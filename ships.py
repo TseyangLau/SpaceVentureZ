2e4900da9d16c240c9693d13197cd56fc954809c
du0ng4 <47843171+du0ng4@users.noreply.github.com>
2020-09-16 16:31:11 -0700

    Update ships.py

--- ships.py
+++ ships.py
@@ -7,14 +7,11 @@ class Player(Ship):
         super(Player, self).__init__(hp, x, y, speed)
 
     def draw(self, window):
-        pygame.Color.r
-        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
-
+        pygame.draw.rect(window, (0, 0, 255), (self.x, self.y, 50, 50))
 
 class Enemy(Ship):
     def __init__(self, hp, x, y, speed):
         super(Enemy, self).__init__(hp, x, y, speed)
 
     def draw(self, window):
-        pygame.Color.b
         pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 40, 40))
