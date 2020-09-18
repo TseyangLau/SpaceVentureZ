7c1c1564f8aa8cc2ae85332dafd286534aca47d8
isabel2296 <70250018+isabel2296@users.noreply.github.com>
2020-09-17 20:27:42 -0700

    Update main.py
    9.17.2020 removed run_game from main.py since it can be just called in display.py and it was getting to long which made the code look messy, replaced with run() and moved the game objects to the __init__ of  SpaceVentureZ class

--- main.py
+++ main.py
@@ -12,58 +12,22 @@ class SpaceVentureZ(Display):
         # icon = pygame.image.load('...')
         # pygame.display.set_icon(icon)
         # game variables
-        self.playerLifeCount = 100
+        self.playerLifeCount = 100 
+        
+        # instantiated here only for testing
+        self.player_ship = Player(self.playerLifeCount, 400, 400, 10)
+        self.enemy_ship = Enemy(self.playerLifeCount, 200, 200, 10)
+        
+    ''' Runs the game overwrites the run in display.py which is currently empty '''    
+    def run(self):
+        # check screen size in case of resize
+        w, h = pygame.display.get_surface().get_size()
+        keys = pygame.key.get_pressed()
 
-    def run_game(self):
-        start = True
-
-        player_ship = Player(100, 400, 400, 10)
-        enemy_ship = Enemy(100, 200, 200, 10)  # instantiated here only for testing
-
-        # Loops during runtime
-        while start:
-            self.clock.tick(60) # set framerate
-
-            ''' USER INTERACTION '''
-            # check screen size in case of resize
-            w, h = pygame.display.get_surface().get_size()
-
-            # mouse coordinates and keys pressed
-            x, y = pygame.mouse.get_pos()
-            keys = pygame.key.get_pressed()
-
-            ''' MENU SECTION '''
-            self.display.fill((0, 0, 0))
-            self.add_text('Game Page', self.font, (255, 0, 0), self.display, 100, 0)
-
-            # Buttons
-            menu = pygame.Rect(0, 0, 80, 40)
-
-            # if buttons are clicked actions
-            if menu.collidepoint(x, y):
-                if self.user_click:
-                    self.user_click = False
-                    self.menu()
-            # Draw Button
-            pygame.draw.rect(self.display, (192, 192, 192), menu)
-            self.add_text('Menu', self.font, (255, 255, 255), self.display, 0, 5)
-
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    start = False
-                if keys[pygame.K_ESCAPE]:
-                    start = False
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        self.user_click = True
-
-            ''' IN-GAME CODE '''
-            player_ship.movement(keys, (w, h))
-            enemy_ship.draw(self.display)
-            player_ship.draw(self.display)
-            pygame.display.update()
-
+        '''In Game Code'''
+        self.player_ship.movement(keys, (w, h))
+        self.enemy_ship.draw(self.display)
+        self.player_ship.draw(self.display)
 
 # can create other functions here to help run game
 
@@ -74,9 +38,7 @@ if __name__ == '__main__':
     svz.menu()
 
 ''' 
-run_game() in SpaceVentureZ overwrites the run_game in Display()
-Note: place all game instances and functions for the game in run_game()
-there is an example already placed
+run() runs the game initially called in run_game in the Display class
 
 self.display is the window (changed the name from self.window) 
 '''
