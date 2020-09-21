ecdd4acddf3a3b4855b841b484139b267d0382e9
Andy Duong <47843171+du0ng4@users.noreply.github.com>
2020-09-21 16:16:58 -0700

    Update ships.py

--- ships.py
+++ ships.py
@@ -5,16 +5,13 @@ from shipBase import Ship
 class Player(Ship):
     def __init__(self, hp, x, y, speed):
         super(Player, self).__init__(hp, x, y, speed)
-        self.playerModel = pygame.image.load('player.png')
 
     def draw(self, window):
-        window.blit(self.playerModel, (self.x, self.y))
-
+        pygame.draw.rect(window, (0, 0, 255), (self.x, self.y, 50, 50))
 
 class Enemy(Ship):
     def __init__(self, hp, x, y, speed):
         super(Enemy, self).__init__(hp, x, y, speed)
-        self.enemyModel = pygame.image.load('enemy.png')
 
     def draw(self, window):
-        window.blit(self.enemyModel, (self.x, self.y))
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 40, 40))
