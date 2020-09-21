cafc600fc99617da28241d6cfaaa018e92941fd0
Andy Duong <47843171+du0ng4@users.noreply.github.com>
2020-09-21 16:00:30 -0700

    Update shipBase.py

--- shipBase.py
+++ shipBase.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship:
 
     """A class to create the player/enemy ship."""
@@ -14,13 +15,13 @@ class Ship:
         self.laser_img = None
 
     def movement(self, keys, boundary):
-        if keys[pygame.K_LEFT] and self.x > self.move_speed - 10:
+        if keys[pygame.K_LEFT] and self.x > self.move_speed - 20:
             self.x -= self.move_speed
         if keys[pygame.K_RIGHT] and self.x < boundary[0] - 50:
             self.x += self.move_speed
         if keys[pygame.K_UP] and self.y > self.move_speed - 10:
             self.y -= self.move_speed
-        if keys[pygame.K_DOWN] and self.y < boundary[1] - 40 - self.move_speed:
+        if keys[pygame.K_DOWN] and self.y < boundary[1] - 50 - self.move_speed:
             self.y += self.move_speed
 
 
