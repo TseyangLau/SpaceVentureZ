2bd91f2cba90ed155be161c6abf27aa910863761
Alfredo Llamas <14206286+fllamas@users.noreply.github.com>
2020-09-30 21:55:42 -0700

    Update laser.py
    Added more to laser.py to draw it on screen.

--- laser.py
+++ laser.py
@@ -1,31 +1,37 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Laser(Sprite):
     """A class to manage lasers fired from ships."""
 
-    def __init__(self, ai_game):
+    def __init__(self,x, y, display): #ai_game
         """Create a laser object at the ship's current location."""
         super().__init__()
-        self.screen = ai_game.screen
-        self.settings = ai_game.settings
-        self.color = self.settings.laser_color
+        '''the laser'''
+        self.x = x
+        self.y = y
+        #self.laser = None
+        self.screen = display #ai_game.screen
+        #self.settings = ai_game.settings
+        self.color = (255, 0, 0) #self.settings.laser_color
 
-        # Create a laser rect at (0,0) and then set currect position.
-        self.rect = pygame.Rect(0, 0, self.settings.laser_width,
-                                self.settings.laser_height)
-        self.rect.midtop = ai_game.ship.rect.midtop
+        # Create a laser rect at (0,0) and then set current position.
+        # self.x, self.y
+        #
+        self.laser = pygame.Rect(self.x, self.y, 10,10) #spawning
+        #self.rect.midtop = ai_game.ship.rect.midtop
 
         # Store the laser's position as a decimal value.
-        self.y = float(self.rect.y)
+        self.y = float(self.laser.y)
 
     def update(self):
         """Move the laser up the screen"""
         # Update the decimal position of the laser.
-        self.y -= self.settings.laser_speed
+        self.y -= 4 #self.settings.laser_speed
         # Update the rect position
-        self.rect.y = self.y
+        self.laser.y = self.y
 
     def draw_laser(self):
         """Draw the laser to the screen."""
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.color, self.laser)
