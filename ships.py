9413e83850406921d55191d98b6f512831638178
Andy Duong <47843171+du0ng4@users.noreply.github.com>
2020-10-21 20:55:17 -0700

    Update ships.py

--- ships.py
+++ ships.py
@@ -22,15 +22,26 @@ class Player(Ship):
         self.ship.x, self.ship.y, self.ship.w, self.ship.h = self.x, self.y, 64, 64
         window.blit(self.playerModel, (self.x, self.y))
 
+    def movement(self, keys, boundary):
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.x > self.move_speed - 20:
+            self.x -= self.move_speed
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.x < boundary[0] - 50:
+            self.x += self.move_speed
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and self.y > self.move_speed - 10:
+            self.y -= self.move_speed
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.y < boundary[1] - 50 - self.move_speed:
+            self.y += self.move_speed
+
     def fire_laser(self, keys):
+        pygame.key.set_repeat(0, 100)
         if keys[pygame.K_SPACE]:
             '''pygame.key.set_repeat(delay, interval) -> this is in milliseconds'''
             # set delay when the space key is held down
-            pygame.key.set_repeat(500, 500)
+            #pygame.key.set_repeat(1, 100)
             # to print delay, for testing purposes
             print(pygame.key.get_repeat())
-            
-            if len(self.lasers.sprites()) > 11:
+
+            if len(self.lasers.sprites()) >= 10:
                 # I don't know yet
                 pass
             else:
@@ -49,16 +60,18 @@ class Player(Ship):
             laser.draw_laser()
             laser.update()
 
-class Enemy(Ship):
-    # class variable to keep track of all enemy hitboxes
+class Enemy:
+    # class variable to keep track of all enemy hit boxes
     enemies = list()
+    direction = 0
 
     def __init__(self, hp, x, y, speed):
-        super(Enemy, self).__init__(hp, x, y, speed)
         self.enemyModel = pygame.image.load('game_images/Enemy.png')
-        self.direction = random.randint(0, 3)
-        self.change_direction = 0
         self.ship = self.enemyModel.get_rect()  # gets the rect of image
+        self.health = hp
+        self.x = x
+        self.y = y
+        self.move_speed = speed
         Enemy.enemies.append(self)
 
     def __del__(self):
@@ -71,41 +84,62 @@ class Enemy(Ship):
         Enemy.enemies.append(self)
         window.blit(self.enemyModel, (self.x, self.y))
 
-    # follows target within range, otherwise just move randomly
-    def auto_movement(self, target):
-        # check for bot collision here
-        for x in Enemy.enemies:
-            if self.ship != x.ship and self.collision(x.ship):
-                self.direction = (x.direction + 2) % 4
-                self.move_forward()
-            else:
-                dx, dy = target.x - self.x, target.y - self.y
-                dist = math.hypot(dx, dy)
-                # if in detection range
-                if 350 > dist > 0:
-                    dx, dy = dx / dist, dy / dist
-                    self.x += dx * self.move_speed
-                    self.y += dy * self.move_speed
-                else:
-                    self.change_direction = random.randint(0, 100)
-                    if self.change_direction == 1:
-                        self.direction = random.randint(0, 3)
-                    self.move_forward()
+    def auto_movement(self):
+        # don't go offscreen
+        for instance in Enemy.enemies:
+            if instance.x < 0:
+                Enemy.direction = 1
+            if (instance.x + 64) > 750:
+                Enemy.direction = 0
+        # change direction randomly
+        change_direction = random.randint(0, 500)
+        if change_direction == 0:
+            Enemy.direction = random.randint(0, 1)
+        # progress movement
+        if Enemy.direction == 0:
+            self.x -= self.move_speed
+        if Enemy.direction == 1:
+            self.x += self.move_speed
 
-    def move_forward(self, multiplier=1):
-        if self.direction == 0:  # up
-            self.y -= self.move_speed * multiplier
-            if self.y < 5:
-                self.direction = 2
-        elif self.direction == 1:  # right
-            self.x += self.move_speed * multiplier
-            if self.x > 685:
-                self.direction = 3
-        elif self.direction == 2:  # down
-            self.y += self.move_speed * multiplier
-            if self.y > 685:
-                self.direction = 0
-        elif self.direction == 3:  # left
-            self.x -= self.move_speed * multiplier
-            if self.x < 5:
-                self.direction = 1
+    def collision(self, obj2):
+        return self.ship.colliderect(obj2)
+
+
+    # def auto_movement(self, target):
+    #     # check for bot collision here
+    #     for x in Enemy.enemies:
+    #         if self.ship != x.ship and self.collision(x.ship):
+    #             self.direction = (x.direction + 2) % 4
+    #             #self.move_forward()
+    #             x.movement()
+    #         else:
+    #             dx, dy = target.x - self.x, target.y - self.y
+    #             dist = math.hypot(dx, dy)
+    #             # if in detection range
+    #             if 350 > dist > 0:
+    #                 dx, dy = dx / dist, dy / dist
+    #                 self.x += dx * self.move_speed
+    #                 self.y += dy * self.move_speed
+    #             else:
+    #                 self.change_direction = random.randint(0, 100)
+    #                 if self.change_direction == 1:
+    #                     self.direction = random.randint(0, 3)
+    #                 self.move_forward()
+    #
+    # def move_forward(self, multiplier=1):
+    #     if self.direction == 0:  # up
+    #         self.y -= self.move_speed * multiplier
+    #         if self.y < 5:
+    #             self.direction = 2
+    #     elif self.direction == 1:  # right
+    #         self.x += self.move_speed * multiplier
+    #         if self.x > 685:
+    #             self.direction = 3
+    #     elif self.direction == 2:  # down
+    #         self.y += self.move_speed * multiplier
+    #         if self.y > 685:
+    #             self.direction = 0
+    #     elif self.direction == 3:  # left
+    #         self.x -= self.move_speed * multiplier
+    #         if self.x < 5:
+    #             self.direction = 1
