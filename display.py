1c267bba74e0748767c4eb9645e7c0facf6a6447
Andy Duong <47843171+du0ng4@users.noreply.github.com>
2020-10-21 20:53:25 -0700

    Update display.py

--- display.py
+++ display.py
@@ -15,8 +15,8 @@ class Display:
         self.game_speed = 60
         ''' Following are Display variables need for creation of display canvas'''
         self.background_color = (0, 0, 0)
-        self.width = 750 #600
-        self.height = 800 #1050
+        self.width = 750  # 600
+        self.height = 800  # 1050
         self.font = pygame.font.SysFont('times new roman', 30)
 
         self.display = pygame.display.set_mode((self.width, self.height))
@@ -32,8 +32,6 @@ class Display:
         self.options_mouseover_state = False
 
         ''' GAME '''
-        self.running = True
-        self.player_alive = True
 
     '''Prints out text on the screen based on location x , y'''
 
@@ -45,16 +43,15 @@ class Display:
         display.blit(pygame.transform.scale(image, (scale_w, scale_h)), (x, y))
 
     def hovered_image(self, image, image2, scale_w, scale_h, display, x, y):
-        self.add_image(image2,scale_w+10, scale_h+10 , display, x-4, y-4)
-        self.add_image(image, scale_w,scale_h,display, x, y)
+        self.add_image(image2, scale_w+10, scale_h+10, display, x-4, y-4)
+        self.add_image(image, scale_w, scale_h, display, x, y)
 
     '''Display's game menu '''
     def menu(self):
-        self.display = pygame.display.set_mode((600, 600))
         start = True
 
         '''Load's image'''
-        game_Logo = pygame.image.load('game_images/SpaceVentureZ.png').convert()
+        game_logo = pygame.image.load('game_images/SpaceVentureZ.png').convert()
         start_game = pygame.image.load('game_images/startButton.png').convert()
         options = pygame.image.load('game_images/optionButton.png').convert()
         screen = pygame.image.load('game_images/background.png').convert()
@@ -69,18 +66,18 @@ class Display:
 
         '''menu background music'''
         # royalty free sfx from zapsplat.com
-        pygame.mixer.music.load('game_audio/bgm.wav')  # load menu bgm
-        pygame.mixer.music.set_volume(0.6)
-        pygame.mixer.music.play()  # play menu bgm
+        #pygame.mixer.music.load('game_audio/bg_menu.wav')  # load menu bgm
+        #pygame.mixer.music.set_volume(0.6)
+        #pygame.mixer.music.play()  # play menu bgm
 
         while start:
             self.display.fill((0, 0, 0))
 
             '''Display Image logo AND Button images'''
-            self.add_image(screen, 600, 600, self.display, 0, 0)
-            self.add_image(game_Logo, 450, 350, self.display, 75, 0)
-            self.add_image(start_game, 250, 85, self.display, 175, 200)
-            self.add_image(options, 250, 88, self.display, 175, 288)
+            self.add_image(screen, self.width, self.height, self.display, 0, 0)
+            self.add_image(game_logo, 450, 350, self.display, self.width/5, 0)
+            self.add_image(start_game, 250, 85, self.display, self.width/3, 200)
+            self.add_image(options, 250, 88, self.display, self.width/3, 288)
 
             '''gets user mouse click coordinates '''
             x, y = pygame.mouse.get_pos()
@@ -88,9 +85,9 @@ class Display:
 
             '''Gets the rect from the images loaded and sets the position on display'''
             start_game_b = start_game.get_rect()
-            start_game_b.x, start_game_b.y , start_game_b.w, start_game_b.h = (175, 200, 250, 85)
+            start_game_b.x, start_game_b.y, start_game_b.w, start_game_b.h = (self.width/3, 200, 250, 85)
             options_b = options.get_rect()
-            options_b.x, options_b.y, options_b.w, options_b.h = (175, 288, 250, 88)
+            options_b.x, options_b.y, options_b.w, options_b.h = (self.width/3, 288, 250, 88)
 
             '''get mouseover state'''
             start_mouseover = start_game_b.x + start_game_b.w > mouse[0] > start_game_b.x and start_game_b.y + start_game_b.h > mouse[1] > start_game_b.y
@@ -103,13 +100,13 @@ class Display:
                     start = False
                 if event.type == MOUSEMOTION:
                     if start_mouseover and self.start_mouseover_state is False:
-                        self.hovered_image(start_game, white_back, 250, 85, self.display, 175, 200)
+                        self.hovered_image(start_game, white_back, 250, 85, self.display, self.width/3, 200)
                         menu_mouseover_sound.play()  # play mouseover sfx
                         self.start_mouseover_state = True
                     if start_mouseover is False and self.start_mouseover_state is True:
                         self.start_mouseover_state = False  # reset mouseover
                     if options_mouseover and self.options_mouseover_state is False:
-                        self.hovered_image(options, white_back, 250, 88, self.display, 175, 288)
+                        self.hovered_image(options, white_back, 250, 88, self.display, self.width/3, 288)
                         menu_mouseover_sound.play()  # play mouseover sfx
                         self.options_mouseover_state = True
                     if options_mouseover is False and self.options_mouseover_state is True:
@@ -133,23 +130,22 @@ class Display:
             self.clock.tick(60)
 
     def run_game(self):
-        self.display = pygame.display.set_mode((self.width, self.height))
 
         '''in-game background music'''
         # royalty free sfx from zapsplat.com
-        pygame.mixer.music.load('game_audio/bgm2.wav')  # load game bgm
-        pygame.mixer.music.set_volume(0.5)
-        pygame.mixer.music.play()  # play game bgm
+        #pygame.mixer.music.load('game_audio/bg_game.wav')  # load game bgm
+        #pygame.mixer.music.set_volume(0.5)
+        #pygame.mixer.music.play()  # play game bgm
 
-        while self.running:
+        running = True
+        while running:
             self.display.fill((0, 0, 0))
-            #self.add_text('Game Page', self.font, (255, 0, 0), self.display, 100, 0)
 
             '''pygame event handling, exit and user mouse click handling'''
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
-                    self.running = False
+                    running = False
                     exit()
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
@@ -165,7 +161,6 @@ class Display:
 
     ''' Display's Options Page'''
     def options(self):
-        self.display = pygame.display.set_mode((600, 600))
         start = True
         while start:
             self.display.fill((0, 0, 0))
@@ -194,14 +189,24 @@ class Display:
                                 '''sounds goes here'''
                                 self.menu()
             pygame.display.update()
-            #self.clock.tick(60)
+            self.clock.tick(60)
 
     '''added run here to be overwritten in the main.py'''
     def run(self):
         pass
 
-    '''for pausing the game a work in progress press u key to upause'''
+    '''for pausing the game a work in progress'''
     def paused(self):
+        text = pygame.font.SysFont('times new roman', 100)
+        self.add_text("PAUSED", text, (255, 0, 0), self.display, self.width / 4, self.height / 4)
+
+        restart = pygame.Rect(self.width/3, self.height/2, 100, 40)
+        pygame.draw.rect(self.display, (192, 192, 192), restart)
+        self.add_text('Restart', self.font, (0, 0, 0), self.display, self.width/3, self.height/2)
+
+        options = pygame.Rect(self.width/3+100, self.height/2, 100,40)
+
+        pygame.display.update()
         while self.pause:
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -211,11 +216,6 @@ class Display:
                     if event.key == pygame.K_u:
                         self.pause = False
                         pygame.mixer.music.unpause()  # unpause bgm
-                    
-
-            #pygame.display.update()
-            self.clock.tick(15)
-
 
 
 '''
