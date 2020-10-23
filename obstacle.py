77882be007cfef13559b3a92edceddc3d3111749
isabel2296 <70250018+isabel2296@users.noreply.github.com>
2020-10-22 18:42:01 -0700

    Update obstacle.py
    Created a base obstacle class and modified current black-hole class to incorporate inheritance of base class. 
    Created a new obstacle called Asteroids, in the base class added a collision function that returns true if element touches obstacle. Also in base class added movement for the obstacles that move the obstacle around the display.

--- obstacle.py
+++ obstacle.py
@@ -1,37 +1,56 @@
-from ships import *
+from ships from display import *
+import random 
 
+'''Obstacle cvbe Class for obstacles in the game 
+   Inherits display to keep the obstacles within the boundaries 
+   of the game display handles obstacles movement as well as collisions'''
+class Obstacle(Display):
+    def __init__(self, x, y, w=25, h=25):
+        Display.__init__(self)
+        self.x, self.y = x, y
+        self.w, self.h = w, h
+        self.window = self.display
+        self.ob_rect = None
+        self.speed = random.randint(2, 3)
+        self.dx, self.dy = self.speed, self.speed
 
+    '''checks collision of element with object'''
+    def coll(self, element):
+        return self.ob_rect.colliderect(element)
+
+    ''' moves the object on display bounces off walls'''
+    def move(self):
+        if self.x >= self.width - self.w or self.x <= 0:
+            self.dx = -self.dx
+        self.x += self.dx
+        if self.y >= self.height-500 - self.h or self.y <= 50:
+            self.dy = -self.dy
+        self.y += self.dy
+        
 class BlackHole:
     def __init__(self, x, y, radius, display):
-        self.x = x
-        self.y = y
+        Obstacle.__init__(self, x, y)
+        '''size of black-hole'''
+        self.w = 80
+        self.h = 80
+        '''image handling'''
+        self.bh_image = pygame.image.load('game_images/black-hole.png')
+        self.bh_pulse = pygame.image.load('game_images/black-hole-pulse.png')
 
-        self.window = display
-        self.radius = radius
-        self.captured = []
-
-        self.bh = None
 
     ''' draws the black-hole on the display'''
 
     def draw(self):
+        self.ob_rect = self.bh_image.get_rect()
         self.bh = pygame.draw.circle(self.window, (0, 255, 0), (self.x, self.y), self.radius)
         #self.window.blit(pygame.transform.scale(self.black_hole_img, (self.w, self.h)), (self.x, self.y))
-    ''' checks if the element was captured by black-hole'''
-
-    def is_captured(self, element):
-        return True if element in self.captured else False
-
-    ''' moves the black-hole down the display'''
-
-    def move(self):
-        pass
 
     ''' black-hole pulses when an objects is captured'''
 
     def pulse(self):
-        pygame.draw.circle(self.window, (255, 0, 0), (self.x, self.y), self.radius+20)
-        #self.window.blit(pygame.transform.scale(self.black_hole_img2, (self.w+10, self.h+10)), (self.x, self.y))
+        bh_pulse_rect = self.bh_pulse.get_rect()
+        bh_pulse_rect.x, bh_pulse_rect.y, bh_pulse_rect.w, bh_pulse_rect.h = self.x, self.y, self.w + 10, self.h + 10
+        self.window.blit(self.bh_pulse, bh_pulse_rect)
         '''pulse sfx'''
         # royalty free sfx from zapsplat.com
         black_hole_sound = pygame.mixer.Sound('game_audio/black_hole.wav')  # load sfx
@@ -40,5 +59,25 @@ class BlackHole:
     '''returns true if the element entered within the surface area of the blackhole'''
 
     def entered_bh(self, element):
-        return self.bh.colliderect(element)
+        return self.coll(element)
 
+    
+class Asteroids(Obstacle):
+    def __init__(self, x, y):
+        Obstacle.__init__(self, x, y)
+        '''temp until image is loaded'''
+        self.color = (254, 7, 0)
+        self.hp = 5
+        '''image and rect handling'''
+        self.as_image = None
+    '''draws the asteroids on display'''
+    def draw(self):
+        self.ob_rect = pygame.Rect(self.x, self.y, self.w, self.h)
+        # self.ob_rect = self.as_image.get_rect()
+        # self.ob_rect.x, self.ob_rect.y, self.ob_rect.w, self.ob_rect.h = self.x, self.y, self.w, self.h
+        # self.window.blit(self.as_image, self.ob_rect)
+        pygame.draw.rect(self.window, self.color, self.ob_rect)
+
+    '''explosion? image to be used when removing include sound here'''
+    def explode(self):
+        pass
