3012a483d304dac1acc5d4fd7fb159474e441e20
Yujin Chung <23323904+yuichu@users.noreply.github.com>
2020-11-12 00:56:05 -0800

    added star point text img to player score ui

--- main.py
+++ main.py
@@ -5,36 +5,34 @@ from obstacle import *
 class SpaceVentureZ(Display):
     def __init__(self):
         Display.__init__(self)  # keeps the inheritance of Display
-
         # game variables life for entities
         self.playerHealthPoints = 100
         self.enemyHealthPoints = 20
 
-        # score variable
+        # score aka Star Points variable
         self.score = 0
         self.is_enemy_hit = False
         self.is_enemy_dead = False
+        self.score_text = pygame.image.load('game_images/star-points-text.png').convert()
 
         # temp invincibility variables
         self.collision_invincibility = False
         self.collision_time = 0
         '''creation of entities'''
         self.player_ship = Player(self.playerHealthPoints, 400, 400, 10, self.display)  # added display
-        self.black_hole = BlackHole(random.randrange(100, self.width - 100, 64), random.randrange(100, 400, 64))
-
-        # ASTEROID SPAWNING
+        self.black_hole = BlackHole(self.width/2, self.height-450)
         self.asteroids = [Asteroids(random.randint(100, self.width - 100), random.randint(100, self.height - 500)) for x in range(10)]
 
         # ENEMY SPAWNING
         #testing enemy2 class
-        self.enemy2 = [Enemy2(self.enemyHealthPoints, random.randint(0, self.width-200), random.randint(0, self.height - 200), random.randint(3, 8)) for x in range(10)]
+        #self.enemy2 = [Enemy2(self.enemyHealthPoints, random.randint(0, self.width-200), random.randint(0, self.height - 200), random.randint(3, 8)) for x in range(3)]
 
         self.enemy_max = 50
         self.enemies = list()
         for x in range(10):
             # get y spawn
             y_spawn = random.randrange(0, self.height - 200, 64)
-            while y_spawn == self.black_hole.y or y_spawn == self.player_ship.y:
+            while y_spawn == self.black_hole.y or (350 < y_spawn < 450):
                 y_spawn = random.randrange(0, self.height - 200, 64)
             # spawn enemy
             self.enemies.append(Enemy(self.enemyHealthPoints, random.randrange(0, self.width - 200, 64), y_spawn, 2))
@@ -50,31 +48,34 @@ class SpaceVentureZ(Display):
         self.player_ship.draw(self.display)
         ''' invincibility handling '''
 
-
-
-        ''' BLACK HOLE HANDLING '''
+        ''' BLACK HOLE Drawing '''
         self.black_hole.draw()
+
+        ''' Asteroid Drawing '''
         for x in self.asteroids:
             x.draw()
             x.move()
             if x.hp <= 0:
                 self.asteroids.remove(x)
+            '''aestroid handling with player ship'''
+            if x.coll(self.player_ship.ship):
+                self.player_ship.health -= 10
 
         for x in self.enemies:
             x.draw(self.display)
-        # for x in self.enemy2:
-        #     x.draw()
+        #for x in self.enemy2:
+        #    x.draw()
         '''draws paused on display and handles unpause'''
         if self.pause:
             self.paused()
 
 
-        ''' ENEMY HANDLING && collision handling '''
-        #@tseyang moved your collision detection to before it enteres the enemy handling for loop
-        if self.collision_invincibility:  # added this line idk how to explain but it needs to be here >.<
-            if pygame.time.get_ticks() > self.collision_time:  # time in ms (milliseconds) 3000 == 3 seconds #changed this a little bit - isabel
-                self.collision_invincibility = False
+        ''' ENEMY HANDLING && Collision handling '''
+        if pygame.time.get_ticks() > self.collision_time:
+            self.collision_invincibility = False
         if self.collision_invincibility == False:
+            #for x in self.enemy2:
+            #    x.track(self.player_ship)
             for x in self.enemies:
                 x.auto_movement()
                 #x.track(self.player_ship) # this is for enemy2 ships
@@ -83,20 +84,13 @@ class SpaceVentureZ(Display):
                     self.enemies.remove(x)
                     self.is_enemy_dead = True
 
-                #this block of code handles the collision with the ship, effects on health, and invincibility on hit.
-                # @tseyang moved it to before the for loop above ^^
-
                 if x.collision(self.player_ship.ship):
                     """if enemy ship collides with player ship, then player ship loses hp"""
                     #print("ships touching")
                     self.player_ship.health -= 30
-                    # if x.health <= 0: # shouldnt be here, i moved it back up - isabel
-                    #     self.enemies.remove(x)
-                    #     self.is_enemy_dead = True
+
                     self.collision_invincibility = True
-                #if self.collision_invincibility == True: # dont need this line its already true with the line above
-                    self.collision_time = pygame.time.get_ticks() + 3000 # this is were you added the 3 seconds of wait to your collision time
-                    #break # might not need this - isabel (ended not needed it)
+                    self.collision_time = pygame.time.get_ticks() + 3000
 
                     '''black-hole handling for enemy ships currently removes enemy or teleports by random chance'''
                     if self.black_hole.entered_bh(x.ship):
@@ -134,9 +128,11 @@ class SpaceVentureZ(Display):
                 if x.coll(p_laser.laser):
                     x.hp -= 5
 
+
         '''Player Score'''
         # draw score on screen
-        self.add_text("Score " + str(self.score), self.font, (255, 255, 255), self.display, 350, 0)
+        self.add_image(self.score_text, 213, 30, self.display, 230, 5)
+        self.add_text(str(self.score), self.font, (255, 255, 255), self.display, 460, 5)
         # add to score on enemy kill
         if (self.is_enemy_hit is True) and (self.is_enemy_dead is True):
             self.score += 100
@@ -174,14 +170,15 @@ class SpaceVentureZ(Display):
         # Reset score
         self.score = 0
         # Reset states
-        self.enemy_hit = False
+        self.is_enemy_hit = False
+        self.is_enemy_dead = False
         self.collision_invincibility = False
         # Add entities
         self.player_ship = Player(self.playerHealthPoints, 400, 400, 10, self.display)  # added display
         self.black_hole = BlackHole(random.randrange(100, self.width - 100, 64), random.randrange(100, 400, 64))
         self.asteroids = [Asteroids(random.randint(100, self.width - 100), random.randint(100, self.height - 500)) for x in range(10)]
-        self.enemy2 = [Enemy2(self.enemyHealthPoints, random.randint(0, self.width - 200), random.randint(0, self.height - 200),
-                              random.randint(3, 10)) for x in range(10)]
+       #self.enemy2 = [Enemy2(self.enemyHealthPoints, random.randint(0, self.width - 200), random.randint(0, self.height - 200),
+        #                      random.randint(3, 10)) for x in range(3)]
         for x in range(10):
             # get y spawn
             y_spawn = random.randrange(0, self.height - 200, 64)
@@ -194,6 +191,7 @@ class SpaceVentureZ(Display):
 if __name__ == '__main__':
     # create SpaceVentureZ game instance and run it
     svz = SpaceVentureZ()
+    #svz.menu()
     svz.run_game()
 
 ''' 
