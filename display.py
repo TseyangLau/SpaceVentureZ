3597275cb55f38d985325e8c95c2a11f2e891f4e
Yujin Chung <23323904+yuichu@users.noreply.github.com>
2020-12-11 13:03:10 -0800

    for consistency with code on floobits server

--- display.py
+++ display.py
@@ -7,8 +7,9 @@ class Display:
 
     def __init__(self):
         """ initialize pygame and creates game title"""
-        # pygame.mixer.pre_init(44100, -16, 1, 512)  # default
-        pygame.mixer.pre_init(20000, -16, 1, 512)
+        pygame.mixer.pre_init(44100, -16, 1, 512)
+        #pygame.mixer.pre_init(20000, -16, 1, 512)  # option 2 - decent sound quality
+
         pygame.init()
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('SpaceVentureZ')
@@ -37,7 +38,7 @@ class Display:
 
         '''variable that handles when the game is over'''
         self.game_over = False
-        
+
         '''background that is going to be used for scrolling'''
         self.bgimage = pygame.image.load('game_images/sv_bg2.png')
         self.rectBGimg = self.bgimage.get_rect()
@@ -69,17 +70,16 @@ class Display:
         '''Load's image'''
         game_logo = pygame.image.load('game_images/SpaceVentureZ.png').convert()
         start_game = pygame.image.load('game_images/startButton.png').convert()
-        #options = pygame.image.load('game_images/optionButton.png').convert()
-        screen = pygame.image.load('game_images/background.png').convert()
+        screen = pygame.image.load('game_images/space_back.jpg').convert()
         white_back = pygame.image.load('game_images/white_back.png').convert()
         star_point = pygame.image.load('game_images/star-point.png').convert()
 
         '''load sfx'''
         # royalty free sfx from zapsplat.com
         menu_onclick_sound = pygame.mixer.Sound('game_audio/menu_click.wav')
-        menu_onclick_sound.set_volume(0.8)
+        menu_onclick_sound.set_volume(0.5)
         menu_mouseover_sound = pygame.mixer.Sound('game_audio/menu_mouseover.wav')
-        menu_mouseover_sound.set_volume(0.7)
+        menu_mouseover_sound.set_volume(0.5)
 
         '''load bgm'''
         # royalty free sfx from zapsplat.com
@@ -95,21 +95,16 @@ class Display:
             self.add_image(screen, self.width, self.height, self.display, 0, 0)
             self.add_image(game_logo, 450, 350, self.display, self.width/5, 0)
             self.add_image(start_game, 250, 85, self.display, self.width/3, 200)
-            #self.add_image(options, 250, 88, self.display, self.width/3, 288)
 
             '''gets user mouse click coordinates '''
             x, y = pygame.mouse.get_pos()
-            mouse = pygame.mouse.get_pos() #this is the same as the one above.. why repeat?
 
             '''Gets the rect from the images loaded and sets the position on display'''
             start_game_b = start_game.get_rect()
             start_game_b.x, start_game_b.y, start_game_b.w, start_game_b.h = (self.width/3, 200, 250, 85)
-            # options_b = options.get_rect()
-            # options_b.x, options_b.y, options_b.w, options_b.h = (self.width/3, 288, 250, 88)
 
             '''get mouseover state'''
-            start_mouseover = start_game_b.x + start_game_b.w > mouse[0] > start_game_b.x and start_game_b.y + start_game_b.h > mouse[1] > start_game_b.y
-            #options_mouseover = options_b.x + options_b.w > mouse[0] > options_b.x and options_b.y + options_b.h > mouse[1] > options_b.y
+            start_mouseover = start_game_b.x + start_game_b.w > (x, y)[0] > start_game_b.x and start_game_b.y + start_game_b.h > (x, y)[1] > start_game_b.y
 
             '''pygame event handling, exit, mouseover, and user mouse click handling'''
             for event in pygame.event.get():
@@ -124,12 +119,7 @@ class Display:
                         self.start_mouseover_state = True
                     if start_mouseover is False and self.start_mouseover_state is True:
                         self.start_mouseover_state = False  # reset mouseover
-                    # if options_mouseover and self.options_mouseover_state is False:
-                    #     self.hovered_image(options, white_back, 250, 88, self.display, self.width/3, 288)
-                    #     menu_mouseover_sound.play()  # play mouseover sfx
-                    #     self.options_mouseover_state = True
-                    # if options_mouseover is False and self.options_mouseover_state is True:
-                    #     self.options_mouseover_state = False  # reset mouseover
+
                 if event.type == MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.user_click = True
@@ -140,12 +130,7 @@ class Display:
                                     menu_onclick_sound.play()  # play menu click sfx
                                 pygame.mixer.music.stop()  # stop menu bgm
                                 self.restart()
-                        # if options_b.collidepoint(x, y):
-                        #     if self.user_click:
-                        #         self.user_click = False
-                        #         menu_onclick_sound.play()  # play menu click sfx
-                        #         pygame.mixer.music.stop()  # stop menu bgm
-                        #         self.options()
+
             pygame.display.update()
             self.clock.tick(60)
 
@@ -180,37 +165,7 @@ class Display:
             pygame.display.update()
             self.clock.tick(self.game_speed)
 
-    ''' Display's Options Page'''
-    def options(self):
-        start = True
-        while start:
-            self.display.fill((0, 0, 0))
-            self.add_text('Options Page', self.font, (255, 0, 0), self.display, 100, 0)
 
-            # mouse coordinates
-            x, y = pygame.mouse.get_pos()
-
-            '''Creates Buttons and draw's them on the display'''
-            menu = pygame.Rect(0, 0, 80, 40)
-
-            pygame.draw.rect(self.display, (192, 192, 192), menu)
-            self.add_text('Menu', self.font, (255, 255, 255), self.display, 0, 5)
-
-            '''pygame event handling, exit and user mouse click handling'''
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    start = False
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        self.user_click = True
-                        if menu.collidepoint(x, y):
-                            if self.user_click:
-                                self.user_click = False
-                                '''sounds goes here'''
-                                self.menu()
-            pygame.display.update()
-            self.clock.tick(60)
 
     '''added run here to be overwritten in the main.py'''
     def run(self):
@@ -233,10 +188,10 @@ class Display:
         res_rect.x, res_rect.y, res_rect.w, res_rect.h = re_rect.x+res_rect.w+50, self.height/3 + 50, 196, 80
         self.display.blit(resume, res_rect)
 
-        settings = pygame.image.load("game_images/settings.png")
-        set_rect = settings.get_rect()
-        set_rect.x, set_rect.y, set_rect.w, set_rect.h = 0, 0, 40, 40
-        self.display.blit(settings, set_rect)
+        # settings = pygame.image.load("game_images/settings.png")
+        # set_rect = settings.get_rect()
+        # set_rect.x, set_rect.y, set_rect.w, set_rect.h = 0, 0, 40, 40
+        # self.display.blit(settings, set_rect)
 
         sound_text = pygame.image.load("game_images/sound-text.png")
         sound_rect = sound_text.get_rect()
@@ -272,15 +227,11 @@ class Display:
                 if event.type == MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.user_click = True
-                        if res_rect.collidepoint(x, y):
+                        if res_rect.collidepoint(x, y): # reset button handling
                             self.pause = False
                             self.user_click = False
                             pygame.mixer.music.set_volume(0.5)  # increase music volume
-                        if set_rect.collidepoint(x, y):
-                            self.pause = False
-                            self.user_click = False
-                            self.options()
-                        if re_rect.collidepoint(x,y):
+                        if re_rect.collidepoint(x,y): # resume button handling
                             self.pause = False
                             self.user_click = False
                             self.restart()
@@ -330,7 +281,7 @@ class Display:
             pygame.mixer.music.load('game_audio/end_defeat.wav')
             pygame.mixer.music.set_volume(0.3)
             pygame.mixer.music.play()  # play defeat sfx
-        
+
         pygame.display.update()
         while self.game_over:
             x, y = pygame.mouse.get_pos()
@@ -349,7 +300,7 @@ class Display:
 
     def restart(self):
         pass
-    
+
     def update_background(self):
         """Handles all the movement of the background. Decrements y-coordinates every re-draw."""
         self.bgY1 += self.moving_speed
